ebacd6b83f251d9fd50cb04bb00d2b52aedd91f9
arianacatani <mayerlicatani@gmail.com>
2023-06-20 09:57:10 -0500

    ar

--- libro2.java
+++ libro2.java
@@ -1,7 +1,7 @@
 public class libro2 extends LIBRERIA1 {
     String editorial;
 
-    public libro2 (int cod,String autor,String nombrel,int anio,String editorial) {
+    public libro2 (int cod,String autor,String nombrel,int anio) {
         super(cod,autor,nombrel,anio);
         this.editorial=editorial;
     }
--- menu.java
+++ menu.java
@@ -1,6 +1,6 @@
 public class menu {
     public static void main(String[] args){
-        LIBRERIA1 Elicoptero=new libro2(15613,"Maria Angeles","ECLIPSE",2021,"gfd");
+        LIBRERIA1 Elicoptero=new libro2(15613,"Maria Angeles","ECLIPSE",2021);
         LIBRERIA1 la_razon_de_estar_contigo= new LIBRERIA1(15615,"Ariana Catani","ZOMBIE",2012);
 
         System.out.println(LIBRERIA1.getNombrel());
